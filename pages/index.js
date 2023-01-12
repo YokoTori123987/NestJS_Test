0c40a45b19cb903bba56a7fb5ee32df95f916ece
YokoTori123987 <nongtaop1@gmail.com>
2023-01-13 00:54:05 +0700

    start next JS

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class index extends Component {
+  render() {
+    return (
+      <div>index
+        <h1></h1>
+      </div>
+    )
+  }
+}
+
+export default index
